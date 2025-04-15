273ea5dc44e7e8d9e247d0abb0ca37d844bfe588
hedijehane <hediiijehane@gmail.com>
2025-04-15 18:27:32 +0100

    Initial commit

--- Intranet.Application/Interfaces/IUserRepository.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Intranet.Application.Interfaces;
-
-public interface IUserRepository
-{
-    Task<User> GetByEmailAsync(string email);
-    Task AddAsync(User user);
-}
--- Intranet.Domain/Entities/User.cs
+++
@@ -1,8 +0,0 @@
-﻿// Intranet.Domain/Entities/User.cs
-public class User
-{
-    public int Id { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-  
-}
